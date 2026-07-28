952222f31af879d42321a8a5a7d63fb9b38a1847
Haruw09 <91204233+Haruw09@users.noreply.github.com>
2026-07-29 00:03:13 +0300

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s = "#" + "#".join(s) + "#"
+        i = 0
+        best_left = 0
+        best_right = 0
+        for i in range(len(s)):
+            left = i
+            right = i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+
+            if right - left - 2 > best_right - best_left:
+                best_left = left + 1
+                best_right = right - 1
+
+        palindrom = []
+        for char in s[best_left:best_right + 1]:
+            if char != '#':
+                palindrom.append(char)
+        
+        return ''.join(palindrom)
