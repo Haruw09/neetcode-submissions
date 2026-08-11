08dae20ace9cf26de8ba6e55d4f08be987ad067a
Haruw09 <91204233+Haruw09@users.noreply.github.com>
2026-08-12 00:07:46 +0300

    Add: continuous-subarray-sum - submission-6

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        if len(nums) < 2:
+            return False
+
+        prefix_sum = 0
+        first_remainder_idx = dict()
+        first_remainder_idx[prefix_sum] = -1
+
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            remainder = prefix_sum % k
+            if remainder in first_remainder_idx:
+                if i - first_remainder_idx[remainder] > 1:
+                    return True
+            else:
+                first_remainder_idx[remainder] = i
+
+        return False
+            
